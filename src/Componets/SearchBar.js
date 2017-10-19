73e094203a749a13dba8543fda9f72dd7b315796
maryehuerta <mary.huerta@mavs.uta.edu>
2017-10-18 23:09:04 -0500

    Search Bar

---
+++ src/Componets/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={e => this.onInputChange(e.target.value)}/>
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchedTermChange(term);
+  }
+}
+
+export default SearchBar;
